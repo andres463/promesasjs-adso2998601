b584a654b9dac5480c15938c78efb28e9cb8fcb3
andres463 <andres.06.2006.06@gmail.com>
2025-05-21 14:48:31 -0500

    promesa con error y exito

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 // Funcion para simular la descarga de un archivo, utilizando promesas
 
-function nuevapromesa(){
-    return new Promise((resolve) => {
-        setTimeout(() =>{
-            resolve("◀ promesa retornada con éxito después de 8 segundos...")
-        }, 5000)
-    })
+function promesa2(){
+   const error = false;
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (error) {
+        reject("❌ Ocurrió un error al descargar")
+      } else {
+        resolve("✅ Promesa exitosa en 8 segundos")
+      }
+    }, 8000) 
+  })
 }
 
-console.log(nuevapromesa()
-  .then((resultado) => {
-    console.log(resultado)
-  })
-)
+
+promesa2()
+.then((resultado) => {
+    console.log(resultado);
+})
+.catch((error) => {
+    console.error(error);
+})
