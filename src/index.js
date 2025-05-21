bcbe868488611e4311632cbc8bbdfa54fa507a2c
andres463 <andres.06.2006.06@gmail.com>
2025-05-21 14:14:54 -0500

    configuracion de promesa completo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+
+// Funcion para simular la descarga de un archivo, utilizando promesas
+
+function descargarArchivo(){
+    return new Promise((resolve) => {
+        setTimeout(() =>{
+            resolve("📁 Archivo descargado con exito.... ")
+        }, 5000)
+    })
+}
+
+
+
+
 //Funcion con async
 
 
 async function ejecutar(){
     try {
         console.log("⌛Descarga en proceso...")
-        const respuesta = await 
+        const respuesta = await descargarArchivo()
+        console.log(respuesta)
 
     } catch (error) {
+        console.error("❌ Error al descargar", error)
 
     }
 }
+
+ejecutar()
